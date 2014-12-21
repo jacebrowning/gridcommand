236cf655611d393311ae11aac7156d518886ad86
Jace Browning <jacebrowning@gmail.com>
2014-12-21 17:05:10 -0500

    Completed tutorial part 2

--- gridcommand/games/urls.py
+++ gridcommand/games/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from games import views
 
 urlpatterns = [
     url(r'^games/$', views.game_list),
     url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- gridcommand/games/views.py
+++ gridcommand/games/views.py
@@ -1,58 +1,45 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from games.models import Game
 from games.serializers import GameSerializer
 
 
-class JSONResponse(HttpResponse):
-
-    """An HttpResponse that renders its content into JSON."""
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
-def game_list(request):
-    """List all code games, or create a new game."""
+@api_view(['GET', 'POST'])
+def game_list(request, format=None):
+    """List all games, or create a new game."""
     if request.method == 'GET':
         games = Game.objects.all()
         serializer = GameSerializer(games, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = GameSerializer(data=data)
+        serializer = GameSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def game_detail(request, pk):
-    """Retrieve, update or delete a code game."""
+@api_view(['GET', 'PUT', 'DELETE'])
+def game_detail(request, pk, format=None):
+    """Retrieve, update or delete a game instance."""
     try:
         game = Game.objects.get(pk=pk)
     except Game.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = GameSerializer(game)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = GameSerializer(game, data=data)
+        serializer = GameSerializer(game, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         game.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
