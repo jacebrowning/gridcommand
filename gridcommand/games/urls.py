a5f247096d79eecc867301927673d4cb266dfbe6
Jace Browning <jacebrowning@gmail.com>
2015-01-13 23:11:05 -0500

    Completed tutorial part 6

--- gridcommand/games/urls.py
+++ gridcommand/games/urls.py
@@ -1,27 +1,13 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 from games import views
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^games/$',
-        views.GameList.as_view(),
-        name='game-list'),
-    url(r'^games/(?P<pk>[0-9]+)/$',
-        views.GameDetail.as_view(),
-        name='game-detail'),
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail'),
-])
+router = DefaultRouter()
+router.register(r'games', views.GameViewSet)
+router.register(r'users', views.UserViewSet)
 
-# Login and logout views for the browsable API
-urlpatterns += [
-    url(r'^auth/', include('rest_framework.urls',
-                           namespace='rest_framework')),
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- gridcommand/games/views.py
+++ gridcommand/games/views.py
@@ -1,56 +1,29 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from games.models import Game
 from games.serializers import GameSerializer, UserSerializer
 from games.permissions import IsOwnerOrReadOnly
 
 
-class GameList(generics.ListCreateAPIView):
+class GameViewSet(viewsets.ModelViewSet):
 
-    """List all games, or create a new game."""
-
-    queryset = Game.objects.all()
-    serializer_class = GameSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class GameDetail(generics.RetrieveUpdateDestroyAPIView):
-
-    """Retrieve, update or delete a game instance."""
+    """This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions."""
 
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
 
-
-class UserList(generics.ListAPIView):
-
-    """List all users."""
-
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
-    """Retrieve a user instance."""
+    """This viewset automatically provides `list` and `detail` actions."""
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'games': reverse('game-list', request=request, format=format)
-    })
