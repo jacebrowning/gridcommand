f85d84ac748336c8eea19466c0384c9de84f551a
Jace Browning <jacebrowning@gmail.com>
2015-06-01 23:58:56 -0400

    Add failing test for missing service calls

--- tests/test_all.py
+++ tests/test_all.py
@@ -7,21 +7,43 @@ from .conftest import load
 
 def test_create_game_and_players(client):
 
+    # Attempt to get the games list
+
+    response = client.get('/api/games/')
+    assert 403 == response.status_code
+
     # Create a game
 
     response = client.post('/api/games/')
     assert 201 == response.status_code
     game_url = load(response)['uri']
+    game_start_url = load(response)['start']
 
     response = client.get(game_url)
     assert 200 == response.status_code
     players_url = load(response)['players']
 
+    # Attempt to start without players
+
+    response = client.get(game_start_url)
+    assert False is load(response)['started']
+
+    response = client.post(game_start_url)
+    assert 403 == response.status_code
+
     # Create two players
 
     response = client.post(players_url, data={'code': '1'})
     assert 201 == response.status_code
-    player1_url = load(response)['uri']
+    player_1_url = load(response)['uri']
     response = client.post(players_url, data={'code': '2'})
     assert 201 == response.status_code
-    player2_url = load(response)['uri']
+    player_2_url = load(response)['uri']
+
+    response = client.get(player_1_url)
+    assert  0 ==  load(response)['turn']
+
+    # Start the game
+
+
+
