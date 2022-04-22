b4ca0d5c065a7f30a0c9405d00dc8a72f4fbbfa3
Jace Browning <jacebrowning@gmail.com>
2022-04-22 00:50:37 -0400

    Add row/col to cells

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from enum import Enum
 import random
 from typing import Iterator
 
+
+SIZE = 5
+UNITS = 20
+
 app = Flask(__name__)
 
 
@@ -14,11 +18,14 @@ class Color(Enum):
     GREEN = "success"
     YELLOW = "warning"
 
-    NONE = "light"
+    NONE = "dark"
 
 
 @datafile
 class Cell:
+    row: int
+    col: int
+
     color: Color = Color.NONE
 
     value: int = 0
@@ -47,45 +54,63 @@ class Cell:
 
 
 @datafile
-class Row:
-
-    cols: list[Cell] = field(default_factory=lambda: [Cell() for _ in range(5)])
-
-
-@datafile
 class Board:
 
-    rows: list[Row] = field(default_factory=lambda: [Row() for _ in range(5)])
+    cells: list[Cell] = field(default_factory=list)
 
     def __post_init__(self):
-        for row_index, row in enumerate(self.rows):
-            for col_index, cell in enumerate(row.cols):
-                cell.row = row_index
-                cell.col = col_index
+        if not self.cells:
+            for row in range(SIZE):
+                for col in range(SIZE):
+                    self.cells.append(Cell(row, col))
 
-    def __iter__(self) -> Iterator[Cell]:
-        for row in self.rows:
-            for cell in row.cols:
-                yield cell
+    def __iter__(self) -> Iterator[list[Cell]]:
+        index = 0
+        row = []
+        for cell in self.cells:
+            if cell.row == index:
+                row.append(cell)
+            else:
+                yield row
+                row = [cell]
+                index += 1
+
+    def __getitem__(self, xy) -> Cell:
+        row, col = xy
+        for cell in self.cells:
+            if cell.row == row and cell.col == col:
+                return cell
+        raise LookupError(f"Unknown cell: {xy}")
 
 
-@datafile("data/gamess/{self.number}.yml")
+@datafile("data/games/{self.number}.yml")
 class Game:
 
     number: int
+
     round: int = 0
     board: Board = Board()
 
     def initialize(self):
         with datafiles.frozen():
-            for cell in self.board:
-                p = random.random()
-                if 0 < p < 0.25:
+            units = {Color.BLUE: UNITS, Color.RED: UNITS}
+            cells = {Color.BLUE: [], Color.RED: []}
+            for cell in self.board.cells:
+                p = random.randint(1, 4)
+                if p == 1:
                     cell.color = Color.BLUE
-                    cell.value = int(p * 10 + 1)
-                elif 0.25 < p < 0.5:
+                    cell.value = 1
+                    units[Color.BLUE] -= 1
+                    cells[Color.BLUE].append(cell)
+                elif p == 2:
                     cell.color = Color.RED
-                    cell.value = int(p * 10 + 1)
+                    cell.value = 1
+                    units[Color.RED] -= 1
+                    cells[Color.RED].append(cell)
+            for color, count in units.items():
+                for _ in range(count):
+                    cell = random.choice(cells[color])
+                    cell.value += 1
         self.round = 1
 
 
@@ -106,13 +131,13 @@ def board(number: int):
 @app.post("/games/<int:number>/_cell/<int:row>/<int:col>")
 def cell(number: int, row: int, col: int):
     game = Game(number)
-    cell = game.board.rows[row].cols[col]
+    cell = game.board[row, col]
     return render_template("cell.html", game=game, cell=cell, editing=True)
 
 
 @app.post("/games/<int:number>/_cell/<int:row>/<int:col>/<string:direction>")
 def move(number: int, row: int, col: int, direction: str):
     game = Game(number)
-    cell = game.board.rows[row].cols[col]
+    cell = game.board[row, col]
     cell.move(1, direction)
     return render_template("cell.html", game=game, cell=cell, editing=True)
